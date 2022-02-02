d793d51df9b24c746cea07f2d5e1baf973ad56b7
padd60 <padd60@naver.com>
2022-02-02 11:53:06 +0900

    global layout pattern / fetching data / redirect and rewrite

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,54 @@
-import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch("/api/movies")).json();
+      setMovies(results);
+    })();
+  }, []);
+
+  console.log(movies);
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="container">
+        <Seo title="Home" />
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map((item) => (
+          <div className="movie" key={item.id}>
+            <img
+              src={"https://image.tmdb.org/t/p/w500" + item.poster_path}
+              alt="img"
+            />
+            <h4>{item.original_title}</h4>
+          </div>
+        ))}
+      </div>
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
